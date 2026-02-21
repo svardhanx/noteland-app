4dd78e89a347d232eedbd5e1b6f0263e932fa1f2
vishnu <hikingfaab@gmail.com>
2026-02-21 12:30:39 +0530

    fixed id to be integer from SERIAL in createTable

--- Backend/src/DB/createTables.js
+++ Backend/src/DB/createTables.js
@@ -3,28 +3,30 @@ import db from "./db.js";
 async function createTables() {
   try {
     await db.execute(`CREATE TABLE IF NOT EXISTS users (
-      id SERIAL PRIMARY KEY AUTOINCREMENT,
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
       name TEXT NOT NULL,
       email TEXT NOT NULL UNIQUE,
       password TEXT NOT NULL,
-      is_logged_in BOOLEAN DEFAULT false,
+      is_logged_in INTEGER DEFAULT 0,
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
       )`);
 
     await db.execute(`CREATE TABLE IF NOT EXISTS notes (
-            id SERIAL PRIMARY KEY,
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
             title TEXT NOT NULL,
             content TEXT NOT NULL,
-            user_id INT REFERENCES users(id) ON DELETE CASCADE
+            user_id INTEGER,
+            FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
             );
         `);
 
     await db.execute(`CREATE TABLE IF NOT EXISTS tasks (
-        id SERIAL PRIMARY KEY,
-        note_id INT REFERENCES notes(id) ON DELETE CASCADE,
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        note_id INTEGER,
         task_name TEXT NOT NULL,
-        completed BOOLEAN DEFAULT false
+        completed INTEGER DEFAULT 0,
+        FOREIGN KEY (note_id) REFERENCES notes(id) ON DELETE CASCADE
         );
       `);
 
